34b43ac6e1153e153d8da72ec3f7543c5a049edb
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-09-23 23:43:34 -0400

    add button keyboard shortcuts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ray-dark": "#131313",
+      },
+      fontFamily:{
+        inter :["Inter","sans-serif"]
+      }
+    },
   },
   plugins: [],
 };
